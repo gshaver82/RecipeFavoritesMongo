170bb5bb51055e52a8fd353a1554db175c14bcb1
gshaver <geneshaver@gmail.com>
2020-08-06 02:12:25 -0500

    updated routes

--- routes/api_routes.js
+++ routes/api_routes.js
@@ -1,14 +1,7 @@
 const recipesModeldb = require("../models/recipesModel");
 
 module.exports = function (app) {
-    app.get("/jsonall", async (req, res) => {
-        try {
-            data = await recipesModeldb.find({})
-            res.json(data);
-        } catch (err) {
-            console.log(err);
-        }
-    });
+
 
 
     app.post("/api/create", async (req, res) => {
--- routes/html_routes.js
+++ routes/html_routes.js
@@ -2,6 +2,16 @@ const recipesModel = require("../models/recipesModel");
 var path = require("path");
 
 module.exports = function (app) {
+    
+    app.get("/jsonall", async (req, res) => {
+        try {
+            data = await recipesModeldb.find({})
+            res.json(data);
+        } catch (err) {
+            console.log(err);
+        }
+    });
+
     app.get("*", (req, res) => {
         res.sendFile(path.join(__dirname, "../public/index.html"));
     });
